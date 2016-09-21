78cf19dbefb0375057415e398ed292fcf24caa8c
FLORIS-PC\gebruiker <flowner@hotmail.com>
2016-09-21 13:28:11 +0200

    -Major redo

--- shush/shush.ino
+++ shush/shush.ino
@@ -1,18 +1,38 @@
-//Initialization
-#define micSens A0
+// Initilization
+int sensorPinA = A0; // select the input pin for Analog Mic
+int sensorPinD = 7; // select the input pin for Digital Mic
+int LEDpin = 13; // select the pin for the LED
+int sensorValueA = 0; // variable to store the Analog value coming from the sensor
+int sensorValueD = 0; // variable to store the Digital value coming from the sensor
 
-int led = 13;
-int sensorValue = 0;
+/* ~~FUNCTIONS~~ */
 
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+
+
+
+// Setup
+void setup () 
+{
+  Serial.begin (9600);
+  pinMode(LEDpin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  sensorValue = analogRead(micSens);
-  Serial.println(sensorValue);
+//Main loop
+void loop () 
+{
+  sensorValueA = analogRead (sensorPinA);
+  sensorValueD = digitalRead (sensorPinD);
+  Serial.println ("print analog: " + String(sensorValueA));
+  if (sensorValueD == 1)
+  {
+    Serial.println("TRUE!");
+    digitalWrite(LEDpin, HIGH);
+    delay(500);
+  }
+  else
+  {
+    digitalWrite(LEDpin, LOW);
+  }
   delay(100);
 }
