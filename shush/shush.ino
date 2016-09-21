a077e98939b27a267a5ff14bd434e6c7d98b2691
FLORIS-PC\gebruiker <flowner@hotmail.com>
2016-09-21 11:44:08 +0200

    -Added microphone

--- shush/shush.ino
+++ shush/shush.ino
@@ -1,12 +1,18 @@
+//Initialization
+#define micSens A0
+
 int led = 13;
 int sensorValue = 0;
 
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  sensorValue = analogRead(micSens);
+  Serial.println(sensorValue);
+  delay(100);
 }
